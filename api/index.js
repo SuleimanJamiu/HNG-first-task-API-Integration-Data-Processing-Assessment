7288b9daa8d67c5feaa310a263697bdaaf64fa0e
SuleimanJamiu <solomonola1k@gmail.com>
2026-04-16 14:57:21 +0100

    final commit for deployment

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = require("../router/genderizeRoute")
+const app = express();
+
+require("dotenv").config();
+PORT = process.env.PORT || 3000;
+app.use(express.json());
+
+app.use(router);
